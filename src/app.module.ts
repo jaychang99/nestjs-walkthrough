41356bc6dda9e43746a48448120f108d83673698
jaychang99 <jaychang99@gmail.com>
2024-07-07 07:05:01 +0900

    feat(Cats): add middleware for some routes in catsController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,27 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminController } from 'src/admin/admin.controller';
 import { AccountController } from 'src/account/account.controller';
 import { HttpService } from 'src/http/http.service';
 import { CatsModule } from 'src/cats/cats.module';
+import { LoggerMiddleware } from 'src/common/middleware/logger.middleware';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController, AdminController, AccountController],
   providers: [AppService, HttpService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude('cats') // route to exclude
+      .forRoutes({ path: 'cats/*', method: RequestMethod.GET }); // route for this logger to be activated
+  }
+}
